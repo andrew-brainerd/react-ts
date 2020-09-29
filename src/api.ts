250dcc7cb74c0644338de3674af1f79a077c4917
Andrew Brainerd <drwb333@gmail.com>
2020-09-29 15:44:58 -0400

    Fix ramda types warning

--- src/api.ts
+++ src/api.ts
@@ -8,13 +8,12 @@ export const client = axios.create({
   headers: { 'Content-Type': 'application/json' }
 });
 
-export const getRandomImage = async (tag = 'llama') => {
-  const response = await client.get(`/random?api_key=${GIPHY_API_KEY}&tag=${tag}&rating=g`);
-  console.log(response);
+export const getRandomImage = async (tag = 'turtle'): Promise<string | undefined> => {
+  const response = await client.get(`/random?api_key=${GIPHY_API_KEY}&tag=${tag}`);
   return path(['data', 'data'], response);
 };
 
-export const getImageById = async (id: string) => {
+export const getImageById = async (id: string): Promise<string | undefined> => {
   const response = await client.get(`/${id}?api_key=${GIPHY_API_KEY}`);
   return path(['data', 'data'], response);
 };
